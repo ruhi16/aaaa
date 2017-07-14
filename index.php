3026e9480afd573f4f5caa4de4fa9eafe2c5b2f6
Hari Narayan Das <hndas16@gmail.com>
2017-07-14 06:56:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -11,5 +11,9 @@
   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d29104.011330661087!2d88.24748194450291!3d24.241729229908643!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fbd136518dae25%3A0x3f582c266055ce65!2sJiaganj%2C+West+Bengal!5e0!3m2!1sen!2sin!4v1499682143637" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
   
   </div>
+  
+  <? php
+    echo "Hello, I'm from php!";
+  ?>
 </body>
 </html>
